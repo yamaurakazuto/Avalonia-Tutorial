b204caea53dbae3ac2f89a7b47e459c88e360d62
Kazuto Yamaura <xyst510@iclouc.com>
2026-06-06 23:46:02 +0900

    create ToDoItem

---
+++ Avalonia-todo-tutorial/Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia_todo_tutorial.Models
+{
+    internal class ToDoItem
+{
+    public bool IsChecked { set; get; }
+
+    public string? Content { get; set; }
+
+}
+}
