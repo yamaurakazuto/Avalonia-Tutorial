be75904e1a11b143d227c6c8e05889ee1d51d3f7
Kazuto Yamaura <xyst510@iclouc.com>
2026-06-07 21:04:26 +0900

    add MainViewModel

--- Avalonia-todo-tutorial/ViewModels/MainWindowViewModel.cs
+++ Avalonia-todo-tutorial/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,32 @@
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(AddItemCommand))]
+        public partial string? NewItemContent { get; set; }
+
+        [RelayCommand (CanExecute = nameof(CanAddItem))]
+        private void AddItem()
+        {
+            ToDoItems.Add(new TODoItemViewModel()
+            {
+                Content = NewItemContent
+            });
+
+            NewItemContent = null;
+        }
+
+        [RelayCommand]
+        private void RemoveItem(TodoItemViewModel item)
+        {
+            ToDoItems.Remove(item);
+        }
+
+
+        private bool CanAddItem() => !string.IsNullOrWhiteSpace(NewItemContent);
+
+
+
+        public ObservableCollection<ToDoItemViewModel> ToDoItems { get; } = new ObservableCollection<ToDoItemViewModel>();
     }
 }
