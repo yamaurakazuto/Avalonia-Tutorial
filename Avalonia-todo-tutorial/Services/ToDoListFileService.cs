df9a7025795e5151a2868825a5892fc56100b777
Kazuto Yamaura <xyst510@iclouc.com>
2026-06-08 22:51:18 +0900

    create ToDoListFileService

---
+++ Avalonia-todo-tutorial/Services/ToDoListFileService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Avalonia_todo_tutorial.Models;
+
+namespace Avalonia_todo_tutorial.Services
+{
+    public static class ToDoListFileService
+    {
+        private static string _jsonFileName = 
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Avalonia_todo_tutorial", "MyToDoList.txt");
+
+
+        public static async Task SaveToFileAsync(IEnumerable<ToDoItem> itemToSave)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(_jsonFileName)!);
+
+            using (var fs = File.Create(_jsonFileName))
+            {
+                await JsonSerializer.SerializeAsync(fs, itemToSave);
+            }
+        }
+    }
+}
